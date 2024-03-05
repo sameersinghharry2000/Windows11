6d02f55b3b0f6620711c9bf9bd0234ac29eab287
Sameer Singh <136222372+sameersinghharry2000@users.noreply.github.com>
2024-03-05 22:47:29 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,73 +1,102 @@
 let windowkey = document.getElementById("windows11key");
 let windows11 = document.querySelector(".windows11"); //Full Window
-let mainmenu = document.getElementById("mainmenu"); 
+let mainmenu = document.getElementById("mainmenu");
 
-
-windowkey.addEventListener("click", function(){
-    document.getElementById("mainmenu").classList.toggle("mainmenu2");
+windowkey.addEventListener("click", function () {
+  document.getElementById("mainmenu").classList.toggle("mainmenu2");
 });
 
 let call = document.getElementById("callAllApps");
 
-call.addEventListener("click",function(){
-    document.getElementById("all_applications").style.display="block";
-})
+call.addEventListener("click", function () {
+  document.getElementById("all_applications").style.display = "block";
+});
 
 let backallapps = document.getElementById("backallapps");
 
-backallapps.addEventListener("click",function(){
-    document.getElementById("all_applications").style.display="none";
-})
- document.getElementById("switchoff").addEventListener("click",function(){
-    document.getElementById("switchoptions").classList.toggle("switchoptions2")
- })
+backallapps.addEventListener("click", function () {
+  document.getElementById("all_applications").style.display = "none";
+});
+document.getElementById("switchoff").addEventListener("click", function () {
+  document.getElementById("switchoptions").classList.toggle("switchoptions2");
+});
+
+function TimeBhai() {
+  let hrs = document.getElementById("hrs");
+
+  let currentTimeDate = new Date();
+
+  let currentDate =
+    currentTimeDate.getHours() + ":" + currentTimeDate.getMinutes();
+
+  hrs.innerHTML = currentDate;
+
+  let Datee = document.getElementById("date");
+
+  let currentTime =
+    currentTimeDate.getDate() +
+    "/" +
+    currentTimeDate.getMonth() +
+    "/" +
+    currentTimeDate.getFullYear();
+
+  date.innerHTML = currentTime;
 
 
-let hrs = document.getElementById("hrs");
 
-let currentTimeDate = new Date();
-
-let currentDate = currentTimeDate.getHours() + ":" + currentTimeDate.getMinutes();
-
-hrs.innerHTML = currentDate;
-
-let Datee = document.getElementById("date");
-
-let currentTime = currentTimeDate.getDate() + "/" + currentTimeDate.getMonth() + "/" + currentTimeDate.getFullYear();
-
-date.innerHTML = currentTime;
+}
+TimeBhai();
 
 let tempData = document.getElementById("tempature");
 
 
 let apiKey = "4c42ccc3ac094c96ab117513724150";
-let apiUrl = "http://api.weatherapi.com/v1/current.json?key=4c42ccc3ac094c96ab1175137241501&q=india";
+let apiUrl =
+  "http://api.weatherapi.com/v1/current.json?key=4c42ccc3ac094c96ab1175137241501&q=india";
 
-async function checkWeather(){
-    let response = await fetch(apiUrl);
-    var data = await response.json();
-    
-    console.log(data);
+async function checkWeather() {
 
-                tempData.innerHTML = 
-                    `<img src="${data.current.condition.icon}">
+  let response = await fetch(apiUrl);
+  var data = await response.json();
+  console.log(data);
+
+  tempData.innerHTML = `<img src="${data.current.condition.icon}">
                     <div class="tempaturedata">
                         <h2> ${data.current.temp_c}°C</h2>
                         <h4>${data.current.condition.text}</h4>
-                    </div> `
+                    </div> `;
 
-                    const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+  const month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
 
+  let currentTimeDate = new Date();
+  
+  let name = month[currentTimeDate.getMonth()];
 
-                    let name = month[currentTimeDate.getMonth()];
-
-                tempaturePageBackground.innerHTML = 
-`<div id="tempaturePageBackground" class="tempaturePageBackground">
+  tempaturePageBackground.innerHTML = `<div id="tempaturePageBackground" class="tempaturePageBackground">
                 <div class="col1">
                     <div class="box1">
                         <div class="country">${data.location.name}</div>
                         <div class="date">
-                        ${currentTimeDate.getDate() + " / " + month[currentTimeDate.getMonth()] + " / "+ currentTimeDate.getFullYear() }
+                        ${
+                          currentTimeDate.getDate() +
+                          " / " +
+                          month[currentTimeDate.getMonth()] +
+                          " / " +
+                          currentTimeDate.getFullYear()
+                        }
                         </div>
                     </div>
                     <div class="box2">
@@ -84,18 +113,17 @@ async function checkWeather(){
                 <div class="boxs"><img src="img/weather/clear.png" alt=""><p>Wednesday</p><p>32°C</p></div>
                 <div class="boxs"><img src="img/weather/clear.png" alt=""><p>Thursday</p><p>32°C</p></div>
                 <div class="boxs"><img src="img/weather/rain.png" alt=""><p>Friday</p><p>32°C</p></div>
-                </div>`
+                </div>`;
+    }
 
-}
+
 checkWeather();
 
 let tempaturePage = document.querySelector(".tempaturePage");
 
-function inn(){
-    tempaturePage.style.left="80px";
+function inn() {
+  tempaturePage.style.left = "80px";
 }
-function out(){
-    tempaturePage.style.left="-350px"
+function out() {
+  tempaturePage.style.left = "-350px";
 }
-
-
